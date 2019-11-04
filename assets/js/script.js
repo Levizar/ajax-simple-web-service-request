6a2ee05374736f6b78a38fe694ba33101a88038b
Brice Bartoletti <bartolettibrice@gmail.com>
2019-11-04 22:34:51 +0100

    need to add img crooper

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,14 +9,15 @@
             const author   = await quoteReceived.author;
             const quote    = await `"${quoteReceived.quote}"`;
             const photoSrc = await quoteReceived.photo;
-
-            if (photoSrc == undefined || photoSrc == null || photoSrc == ""){
-                document.querySelector(".crawl img").remove();
-            }
-
+            
             document.getElementById("author").innerHTML = author;
             document.getElementById("quote").innerHTML = quote;
-            document.getElementById("photo").src = photoSrc;
+            if (photoSrc == undefined || photoSrc == null || photoSrc == ""){
+                document.getElementById("#photo").remove();
+            } else {
+                document.getElementById("photo").src = photoSrc;
+            }
+
 
         }catch (error){
             console.error(error); 
